c72c39a4fdef278f03e9ff47248d9d3179fbb769
ellen <124398916@umail.ucc.ie>
2026-02-24 09:33:26 +0000

    h

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 # -----------------------
-# HARD-CODED CONFIG (DEMO ONLY)
+# HARD-CODED CONFIG (DEMO ONLY) jhsdvhjds
 # -----------------------
 API_KEY = ("")
 CITY = "Cork"
